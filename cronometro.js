2c1b1be1f41cdb48040e5085fb9442e9509ed168
cunhaluisg5 <cunhaluisg5@gmail.com>
2019-01-17 12:16:53 -0200

    Implementado cronometro.js

--- cronometro.js
+++ cronometro.js
@@ -1,3 +1,29 @@
 (function(win, doc){
+    
     'use strict';
+
+    var $field = doc.querySelector('[data-js="field"]');
+    var $start = doc.querySelector('[data-js="start"]');
+    var $stop = doc.querySelector('[data-js="stop"]');
+    var $reset = doc.querySelector('[data-js="reset"]');
+    var time;
+
+    $start.addEventListener('click', timerStart, false);
+    $stop.addEventListener('click', timerStop, false);
+    $reset.addEventListener('click', timerReset, false);
+
+    function timerStart(){
+        $field.value = +$field.value + 1;
+        time = setTimeout(timerStart, 1000);
+    }
+
+    function timerStop(){
+        clearTimeout(time);
+    }
+
+    function timerReset(){
+        $field.value = "0";
+        timerStop();
+    }
+
 })(window, document);
